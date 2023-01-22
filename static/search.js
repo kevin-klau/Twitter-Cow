fb968036b19843cbb97f8cf7e1b3ba9f9203c211
N-Hioe <96747830+N-Hioe@users.noreply.github.com>
2023-01-22 08:29:56 -0500

    finaledits

--- static/search.js
+++ static/search.js
@@ -7,3 +7,12 @@ function searching(){
     request.send()
     fetch('http://127.0.0.1:5000/?#/keke')
 }
+
+function test(){
+    document.getElementById("rank1").innerHTML = "London";
+    document.getElementById("rank2").innerHTML = "Toronto";
+    document.getElementById("rank3").innerHTML = "Quebec City";
+    document.getElementById("rank4").innerHTML = "Stockholm";
+    document.getElementById("rank5").innerHTML = "Calgary";
+    document.getElementById("rank6").innerHTML = "Helsinki";
+}
