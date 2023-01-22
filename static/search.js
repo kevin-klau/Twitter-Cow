dd20e56927db0f3c2a9b5ad33bf8d55fee33b495
N-Hioe <96747830+N-Hioe@users.noreply.github.com>
2023-01-22 03:55:04 -0500

    search bar function

---
+++ static/search.js
@@ -0,0 +1,7 @@
+let searchQuery = document.getElementById("searchinput").value;
+
+function searching(){
+    let searchQuery = document.getElementById("searchinput").value;
+    console.log(searchQuery);
+    document.getElementById("searchinput").value='';
+ }
